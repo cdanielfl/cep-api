d02d590327bf5278e9187e97840e289383fdd656
CARLOS DANIEL FREITAS LIMA LINS <carloslins6232@digitalcollege.internal>
2025-07-01 21:44:41 -0300

    Adicionado fetch para estados e munidicipios

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const consultarCep = () => {
-    const cep = document.getElementById('cep').value
+    const cep = document.getElementById('cep').value;
     fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
         .then(response => response.json())
-        .then(json => {console.log(json)
-             document.getElementById('logradouro').value = json.address
-             document.getElementById('bairro').value = json.district
-             document.getElementById('uf').value = json.state
-             
+        .then(json => {
+            console.log(json)
+            document.getElementById('logradouro').value = json.address
+            document.getElementById('bairro').value = json.district
+            document.getElementById('uf').value = json.state
+            // Localidade será preenchida via select de municípios
+            ConsultarMunicipios(json.state);
+
 
         })
+        .catch(error => console.error('Erro:', error));
 }
 
+const ConsultarEstados = () => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`)
+        .then(response => response.json())
+        .then(json => {
+            let options = '<option selected disabled value="">Selecione...</option>'
 
+            json.forEach(estado => {
+                options = options + `<option value="${estado.sigla}">${estado.sigla}</option>`
+            })
+
+            document.getElementById('uf').innerHTML = options
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+ConsultarEstados()
+
+const ConsultarMunicipios = (uf) => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+            let options = '<option selected disabled value="">Selecione...</option>'
+
+            json.forEach(municipio => {
+                options = options + `<option value="${municipio.nome}">${municipio.nome}</option>`
+            })
+
+            document.getElementById('localidade').innerHTML = options
+
+        })
+        .catch(error => console.error('Erro:', error));
+}   
